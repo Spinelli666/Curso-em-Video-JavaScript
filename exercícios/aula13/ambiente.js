1a0f7fca9a7858fe05c64675c6a383a4866b2aa7
Bernardo Gonçalves Spinelli Martins <107887848+Spinelli666@users.noreply.github.com>
2023-11-28 13:48:44 -0300

    Aula 13

---
+++ exercícios/aula13/ambiente.js
@@ -0,0 +1,17 @@
+/*
+Estrutura de Repetição com teste lógico no início.
+
+var c = 1
+while (c <= 5) {
+    console.log(`Passo ${c}`)
+    c++
+}*/
+
+/*
+Estrutura de Repetição com teste lógico no final.
+
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 5)*/
